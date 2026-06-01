93ae41429bfd514ec78f855417dffca597c4df5c
Divyansh <divyansh.vermalocal@gmail.com>
2026-06-02 00:20:43 +0530

    Cors issue resolved

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,19 @@ from app.database.database import engine
 from app.database.base import Base
 from app.database.database import engine
 from app.auth.routes import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
 
