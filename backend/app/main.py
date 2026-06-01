15569c7a20131d95a18ac06aaa0440c657ec4b6d
Divyansh <divyansh.vermalocal@gmail.com>
2026-06-01 13:09:34 +0530

    Setup PostgreSQL database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from app.database.database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def health_check():
-    return {"message": "Incident Management API is running"}
+    return {
+        "message": "Incident Management API is running"
+    }
+
+@app.get("/db-test")
+def db_test():
+    try:
+        connection = engine.connect()
+        connection.close()
+
+        return {
+            "status": "success",
+            "message": "Database connected successfully"
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
