d861015c78a2cc9d6665d030acb7039fa1364152
Divyansh <divyansh.vermalocal@gmail.com>
2026-06-02 11:52:38 +0530

    Implement frontend authentication and route protection

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import MainLayout from "../layouts/MainLayout"
+import ProtectedRoute from "./ProtectedRoutes";
 
 function AppRoutes() {
   return (
@@ -9,7 +10,14 @@ function AppRoutes() {
     <Routes>
         <Route path="/" element={<Login/>}/>
         <Route element= {<MainLayout/>}>
-        <Route path="/dashboard" element = {<Dashboard/>}/>
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
         </Route>
     </Routes>
     </BrowserRouter>
--- frontend/src/routes/ProtectedRoutes.jsx
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -1,5 +1,24 @@
-const ProtectedRoute = () => {
-  return null;
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } =
+    useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
